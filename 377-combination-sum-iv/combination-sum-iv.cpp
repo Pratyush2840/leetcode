0f3c8bff9093509745fa1c93742adcdf02a6b1d9
PRATYUSH SINGH <163348524+Pratyush2840@users.noreply.github.com>
2026-08-19 17:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (53.47%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int target, vector<int>& dp) {
+        if (target == 0)
+            return 1;
+        if (dp[target] != -1)
+            return dp[target];
+        int ans = 0;
+        for (int num : nums) {
+            if (num <= target) {
+                ans += solve(nums, target - num, dp);
+            }
+        }
+        return dp[target] = ans;
+    }
+
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, -1);
+        return solve(nums, target, dp);
+    }
+};
