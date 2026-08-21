0fde6715ba621d72a9104eb30186d8d988c69269
PRATYUSH SINGH <163348524+Pratyush2840@users.noreply.github.com>
2026-08-21 20:09:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (85.90%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
     int nextGreaterElement(int n) {
-        string s = to_string(n);
-        int i = s.size() - 2;
-        while (i >= 0 && s[i] >= s[i + 1]) {
-            i--;
-        }
-        if (i < 0)
-            return -1;
-        int j = s.size() - 1;
-        while (s[j] <= s[i]) {
-            j--;
-        }
-        swap(s[i], s[j]);
-        reverse(s.begin() + i + 1, s.end());
-        long long ans = stoll(s);
-        if (ans > INT_MAX)
-            return -1;
-        return (int)ans;
+        string str = to_string(n);
+        next_permutation(str.begin(), str.end());
+        long long int ans = stoll(str);
+        if(ans > 2147483647) return -1;
+        return ans > n ? ans : -1;
     }
 };
