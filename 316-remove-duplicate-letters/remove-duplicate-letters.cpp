58a60a63e9d7cedbc9a5540b8d9050ab0b15eeb0
PRATYUSH SINGH <163348524+Pratyush2840@users.noreply.github.com>
2026-09-02 19:57:34 +0530

    Time: 1 ms (29.50%) | Memory: 9.8 MB (10.05%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,36 +1,48 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-
         stack<char> st;
         map<char,int> mp;
-        string ans = "";
+        string ans= "";
         vector<int> vis(26,0);
-
         for(auto it : s){
             mp[it]++;
         }
         for(auto it : s){
-            mp[it]--;
-            if(vis[it - 'a'] == 1){
+            if(st.empty()){
+                st.push(it);
+                mp[it]--;
+                vis[it - 'a'] = 1;
                 continue;
             }
-            while(!st.empty() &&
-                  st.top() > it &&
-                  mp[st.top()] > 0){
-
-                vis[st.top() - 'a'] = 0;
-                st.pop();
+            if(it == st.top()){
+                mp[it]--;
+                continue;
             }
-
-            st.push(it);
-            vis[it - 'a'] = 1;
+            if(!st.empty() && it > st.top() && vis[it - 'a'] == 0){
+                st.push(it);
+                mp[it]--;
+                vis[it - 'a'] = 1;
+            }
+            else if(!st.empty() && it < st.top() && vis[it  - 'a'] == 0){
+                //case 1 i can remove the top character
+                while( !st.empty() &&  mp[st.top()] >0  && st.top() > it){
+                    vis[st.top() - 'a'] = 0;
+                    st.pop();
+                }
+                st.push(it);
+                vis[it - 'a'] =1;
+                mp[it]--;
+            }
+            else{
+                mp[it]--;
+            }
         }
         while(!st.empty()){
-            ans += st.top();
+            ans+= (st.top());
             st.pop();
         }
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin() , ans.end());
         return ans;
     }
 };
