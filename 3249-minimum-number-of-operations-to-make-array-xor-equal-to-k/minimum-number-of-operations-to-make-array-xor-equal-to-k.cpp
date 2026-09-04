434018785afe2d192f36c1c23212ec5b799e98ad
PRATYUSH SINGH <163348524+Pratyush2840@users.noreply.github.com>
2026-09-04 13:02:37 +0530

    Time: 96 ms (87.26%) | Memory: 93 MB (46.04%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int ans = 0;
-        int z= k;
-        for(int i = 0 ; i < 32; i++){
-            int cnt  = 0;
-            int target =(z>>i)&1; 
-            for(int j = 0 ; j < nums.size() ; j++){
-                if((nums[j] >> i ) & 1){
-                    cnt++;
-                }
-            }
-            //cnt ood target 1 -> tabh no change
-            //cnt even target zero -> tabh no change
-            //cnt even target 1 -> tabh change hoga
-            //cnt odd tqarget 0 - > tabh change
-            if((cnt &1) == 1 && target ==0){
-                ans++;
-            }
-            else if((cnt &1) == 0 && target == 1){
-                ans++;
-            }
+        int x = k;
+
+        for(int num : nums) {
+            x ^= num;
         }
-        return ans;
+
+        return __builtin_popcount(x);
     }
 };
