973c121c01788c3f0a5534e4903fad72ccd64d0b
PRATYUSH SINGH <163348524+Pratyush2840@users.noreply.github.com>
2026-09-08 11:58:50 +0530

    Time: 390 ms (18.28%) | Memory: 21.4 MB (50.73%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -3,11 +3,10 @@ public:
     int n ;
     int solve(int row,int col , vector<vector<int>>& grid , vector<vector<int>>& dp){
         if(row == n-1)return grid[row][col];
-        
         if(dp[row][col] != 1e9)return dp[row][col];
         int minval = 1e9;
         for(int i=0 ; i< n; i++){
-            int newrow = row + 1;
+            int newrow = row+1 ;
             int newcol = i;
             if(newrow>=0 && newcol>=0 && newrow < n && newcol<n && newcol != col){
                 minval = min(minval , grid[row][col] + solve(newrow,newcol ,grid,dp));
