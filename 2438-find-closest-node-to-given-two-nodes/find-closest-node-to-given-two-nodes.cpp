a4116f09cbb2a558cbd8d6d8f020550c7e8eb104
PRATYUSH SINGH <163348524+Pratyush2840@users.noreply.github.com>
2026-09-11 03:55:27 +0530

    Time: 178 ms (10.01%) | Memory: 165.4 MB (6.70%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,46 +1,48 @@
 class Solution {
 public:
-    int n;
-    
-    void dfs(vector<int>& edges, int startNode, vector<int>& dist_node, vector<bool>& visited) {
-        visited[startNode] = true;
-        
-        int v = edges[startNode];
-        
-        if(v != -1 && !visited[v]) {
-            visited[v] = true;
-            dist_node[v] = 1 + dist_node[startNode];
-            dfs(edges, v, dist_node, visited);
-        }
-    }
-    
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        n = edges.size();
-        
-        vector<int> dist1(n, INT_MAX);
-        vector<int> dist2(n, INT_MAX);
-        vector<bool> visited1(n, false);
-        vector<bool> visited2(n, false);
-        dist1[node1] = 0;
-        dist2[node2] = 0;
-        
-        dfs(edges, node1, dist1, visited1);
-        dfs(edges, node2, dist2, visited2);
-        
-        
-        int minDistNode    = -1;
-        int minDistTillNow = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            
-            int maxD = max(dist1[i], dist2[i]);
-            
-            if (minDistTillNow > maxD) {
-                minDistNode = i;
-                minDistTillNow = maxD;
+        if(node1 == node2) return node1;
+        int n = edges.size();
+        unordered_map<int,int> adj;
+        for(int i = 0; i < n; i++){
+            adj[i] = edges[i];
+        }
+        vector<int> visited(n, -1);
+        vector<int> vis(n, -1);
+        queue<int> q;
+        q.push(node1);
+        visited[node1] = 0;
+        while(!q.empty()){
+            int it = q.front();
+            q.pop();
+            int ad = adj[it];
+            if(ad != -1 && visited[ad] == -1){
+                visited[ad] = visited[it] + 1;
+                q.push(ad);
             }
         }
-
-        return minDistNode;
-        
+        q.push(node2);
+        vis[node2] = 0;
+        while(!q.empty()){
+            int it = q.front();
+            q.pop();
+            int ad = adj[it];
+            if(ad != -1 && vis[ad] == -1){
+                vis[ad] = vis[it] + 1;
+                q.push(ad);
+            }
+        }
+        int ans = -1;
+        int mini = INT_MAX;
+        for(int i = 0; i < n; i++){
+            if(visited[i] != -1 && vis[i] != -1){
+                int dist = max(visited[i], vis[i]);
+                if(dist < mini){
+                    mini = dist;
+                    ans = i;
+                }
+            }
+        }
+        return ans;
     }
 };
