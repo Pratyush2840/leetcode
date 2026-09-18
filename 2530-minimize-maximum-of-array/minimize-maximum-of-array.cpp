0c7bf899e490b92347ea7a429518966dfac0a858
PRATYUSH SINGH <163348524+Pratyush2840@users.noreply.github.com>
2026-09-18 09:14:21 +0530

    Time: 66 ms (12.27%) | Memory: 182.4 MB (8.65%) - LeetSync

--- 2530-minimize-maximum-of-array/minimize-maximum-of-array.cpp
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
-    bool check(long long target, vector<int>& nums) {
-        long long sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            if (sum > target * (i + 1))
+    
+    bool isValid(vector<int> &nums, int mid_max, int n) {
+        
+        vector<long long> arr(begin(nums), end(nums));
+        
+        for(int i = 0; i<n-1; i++) {
+            
+            if(arr[i] > mid_max)
                 return false;
+            
+            long long buffer = mid_max - arr[i];
+            
+            arr[i+1] = arr[i+1] - buffer;   
+            
         }
-        return true;
+        
+        return arr[n-1] <= mid_max;
+        
     }
+    
     int minimizeArrayValue(vector<int>& nums) {
-        long long low = 0;
-        long long high = *max_element(nums.begin(), nums.end());
-        while (low <= high) {
-            long long mid = low + (high - low) / 2;
-            if (check(mid, nums)) {
-                high = mid - 1;
+        int n = nums.size();
+        
+        int maxL = 0;
+        int maxR = *max_element(begin(nums), end(nums));
+        
+        int result;
+        
+        while(maxL <= maxR) {
+            
+            int mid_max = maxL + (maxR-maxL)/2;
+            
+            if(isValid(nums, mid_max, n)) {
+                result = mid_max;
+                maxR   = mid_max-1;
+            } else {
+                maxL = mid_max+1;
             }
-            else {
-                low = mid + 1;
-            }
+            
         }
-        return low;
+        
+        return result;
     }
 };
